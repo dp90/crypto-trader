c7633a467bd23cf309f410b4ed3a6a4fa2d0e27c
David Kroon <davidkroon90@gmail.com>
2021-03-16 22:02:52 +0100

    Delete unnecessary files

--- data_collection/settings.py
+++
@@ -1,7 +0,0 @@
-SETTINGS = {
-    "exchange": "BitStamp",  # TODO: Check names in CoinAPI
-    "timeStep": 1,  # In minutes
-    "cash": "EUR",
-    "cryptoCurrencies": ["BTC", "ADA"],  # TODO: Add currencies
-    "startDate": "01/01/2018"  # TODO: Check date format
-}
